5922619f2fa081b85b7111c0ba0a9f82587f5df0
kushmakar <kushmakarshahu@gmail.com>
2025-07-01 19:52:42 +0545

    auth backend impl

---
+++ app/src/main/java/com/example/smartpark/repository/AuthRepository.kt
@@ -0,0 +1,17 @@
+package com.example.smartpark.repository
+
+interface AuthRepository {
+    //function to register new user(email + password)
+    suspend fun register(email: String, password: String): Result<Unit>
+
+    //function to login existing user
+    suspend fun login(email: String, password: String): Result<Unit>
+
+    suspend fun resetPassword(email: String): Result<Unit>
+
+    suspend fun logout()
+
+    fun isUserLoggedIn(): Boolean
+    fun sendPasswordReset(email: String): Result<Unit>
+
+}
