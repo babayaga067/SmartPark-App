ee4008f490b631384c9829835c73b2da83f8eb8f
kushmakar <kushmakarshahu@gmail.com>
2025-07-31 22:47:22 +0545

    updated to stable version

--- app/src/main/java/com/example/smartpark/model/BookingModel.kt
+++ app/src/main/java/com/example/smartpark/model/BookingModel.kt
@@ -4,7 +4,8 @@ data class BookingModel(
     val id: String = "",
     val userId: String = "",
     val spotId: String = "",
+    val spotName: String = "",
     val price: Int = 0,
     val timestamp: Long = 0L,
-    val status: String = "Active"   // "Active", "Completed", "Cancelled", etc.
+    val status: String = "Active"
 )
